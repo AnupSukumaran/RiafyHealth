51ed73702af417079b2f8f7acaa3d4419a33434d
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-01 17:08:56 +0530

    - first commit

---
+++ RiafyHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiafyHealth
+//
+//  Created by Sukumar Anup Sukumaran on 01/08/20.
+//  Copyright © 2020 Tech_Tonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
